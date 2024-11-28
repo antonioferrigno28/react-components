8ceae0c809cbd8d54adf73e4de5cfe8b52d4b1e8
antonioferrigno28 <ferrigno1@hotmail.it>
2024-11-28 14:08:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <>
+      <h1>ao</h1>
+    </>
+  );
+}
+
+export default App;
